832bf74ec85cb2477d6d789448bfd73cc99bc861
EdreyC <edreycarneiro.profissional@gmail.com>
2021-09-21 12:27:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { useEffect } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container">
+        <img  className="scale-1"className="my-photo" src="https://avatars.githubusercontent.com/u/83123090?v=4" alt="" ></img>
+        <h1 className="blinker">Hello! Welcome<br /> 
+        This is a Editor Text ✍</h1> 
+      
+        <Editor></Editor>
+        </div>
+ 
+        <a className="link"href="https://github.com/EdreyC">Back to my Github</a>
+
+    </div>
+    
+  );
+  }
+
+export default App;
